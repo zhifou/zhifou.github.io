063c8bc95ed076bdc2a39e4ffcc035fc2da9b4e0
zhaoyadong <zhaoyadong@baidu.com>
2024-02-19 19:52:22 +0800

    增加初始化站点

---
+++ rspress.config.ts
@@ -0,0 +1,23 @@
+import * as path from "path";
+import { defineConfig } from "rspress/config";
+
+export default defineConfig({
+    root: path.join(__dirname, "docs"),
+    title: "Rspress",
+    description: "Rspack-based Static Site Generator",
+    icon: "/rspress-icon.png",
+    base: "/",
+    logo: {
+        light: "/rspress-light-logo.png",
+        dark: "/rspress-dark-logo.png",
+    },
+    themeConfig: {
+        socialLinks: [
+            {
+                icon: "github",
+                mode: "link",
+                content: "https://github.com/web-infra-dev/rspress",
+            },
+        ],
+    },
+});
